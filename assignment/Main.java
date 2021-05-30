533105d11dcb049403328b078985904e8590be83
Arliko2002 <77797439+Arliko2002@users.noreply.github.com>
2021-05-30 15:17:53 +0600

    Add files via upload

---
+++ assignment/Main.java
@@ -0,0 +1,25 @@
+package assignment;
+
+import java.security.Key;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        MyHashTable<Integer, String> myHashTable = new MyHashTable<Integer, String>();
+        myHashTable.put(1, "Luffy");
+        myHashTable.put(2, "Nami");
+        myHashTable.put(3, "Usopp");
+        System.out.println(myHashTable.get(1) + " " + myHashTable.get(2) + " " + myHashTable.get(3));
+        System.out.println(myHashTable.remove(2));
+        System.out.println(myHashTable.get(1) + " " + myHashTable.get(2) + " " + myHashTable.get(3));
+
+        System.out.println("----------------------------------------------------------");
+
+        Bst<Integer, Integer> bst = new Bst<Integer, Integer>();
+        bst.put(1, 5);
+        bst.put(2, 6);
+        System.out.println(bst.get(1) + " " + bst.get(2));
+        bst.delete(1);
+        System.out.println(bst.get(1) + " " + bst.get(2));
+    }
+
+}
